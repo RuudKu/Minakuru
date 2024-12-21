bc26a56b342dfc49e24cd8ca1fb67ee36796bde0
Ruud Kuchler <kuru@pggm.nl>
2024-12-21 15:12:08 +0100

    Added bishop move generator

---
+++ DEV/Engine.UnitTests/MoveGenerators/BishopMoveGeneratorTests.cs
@@ -0,0 +1,76 @@
+﻿using Engine.MoveGenerators;
+using FluentAssertions;
+
+namespace Engine.UnitTests.MoveGenerators;
+
+[TestClass]
+public class BishopMoveGeneratorTests
+{
+	[TestMethod]
+	public void FromD4Test()
+	{
+		var fen = "k7/8/8/8/3B4/8/8/7K w - - 0 1";
+		var board = fen.ToBoard();
+
+		var sut = new BishopMoveGenerator();
+
+		var actual = sut.GenerateMove(board, Color.White).ToArray();
+
+		var readableMoves = actual.ToReadableMoves().ToArray();
+		var expected = new ReadableMove[]
+		{
+			// to the left-down
+			new ("d4", "c3", false, false),
+			new ("d4", "b2", false, false),
+			new ("d4", "a1", false, false),
+			// to the left-up
+			new ("d4", "c5", false, false),
+			new ("d4", "b6", false, false),
+			new ("d4", "a7", false, false),
+			// to right-down
+			new ("d4", "e3", false, false),
+			new ("d4", "f2", false, false),
+			new ("d4", "g1", false, false),
+			// to right-up
+			new ("d4", "e5", false, false),
+			new ("d4", "f6", false, false),
+			new ("d4", "g7", false, false),
+			new ("d4", "h8", false, false)
+		};
+
+		readableMoves.Should().BeEquivalentTo(expected);
+	}
+
+	[TestMethod]
+	public void WithOtherPiecesTest()
+	{
+		var fen = "k7/8/5q2/8/3B4/8/8/6K1 w - - 0 1";
+		var board = fen.ToBoard();
+
+		var sut = new BishopMoveGenerator();
+
+		var actual = sut.GenerateMove(board, Color.White).ToArray();
+
+		var readableMoves = actual.ToReadableMoves().ToArray();
+		var expected = new ReadableMove[]
+		{
+			// to the left-down
+			new ("d4", "c3", false, false),
+			new ("d4", "b2", false, false),
+			new ("d4", "a1", false, false),
+			// to the left-up
+			new ("d4", "c5", false, false),
+			new ("d4", "b6", false, false),
+			new ("d4", "a7", false, false),
+			// to right-down
+			new ("d4", "e3", false, false),
+			new ("d4", "f2", false, false),
+			// to-right up
+			new ("d4", "e5", false, false),
+			new ("d4", "f6", true, false)
+		};
+
+		readableMoves.Should().BeEquivalentTo(expected);
+	}
+}
+
---
+++ DEV/MinakuruEngine/MoveGenerators/BishopMoveGenerator.cs
@@ -0,0 +1,59 @@
+﻿namespace Engine.MoveGenerators;
+
+public class BishopMoveGenerator : IMoveGenerator
+{
+	public IEnumerable<Move> GenerateMove(Board board, Color color)
+	{
+		var whitePiecesAt = board.WhiteKing | board.WhiteQueens | board.WhiteRooks | board.WhiteBishops | board.WhiteKnights | board.WhitePawns;
+		var blackPiecesAt = board.BlackKing | board.BlackQueens | board.BlackRooks | board.BlackBishops | board.BlackKnights | board.BlackPawns;
+
+		var bishops = color == Color.White ? board.WhiteBishops : board.BlackBishops;
+		var ownPiecesAt = color == Color.White ? whitePiecesAt : blackPiecesAt;
+		var opponentPiecesAt = color == Color.White ? blackPiecesAt : whitePiecesAt;
+
+		for (byte from = 0; from < 64; from++)
+		{
+			var filter = (ulong)1 << from;
+			if ((bishops & filter) != 0)
+			{
+				int fromColumn = from % 8;
+				int fromRow = from / 8;
+
+				int toColumn;
+				int toRow;
+
+				for (int direction = 0; direction < 4; direction++)
+				{
+					int deltaColumn = new int[] { -1, -1, +1, +1 }[direction];
+					int deltaRow = new int[] { -1, +1, -1, +1 }[direction];
+					toColumn = fromColumn + deltaColumn;
+					toRow = fromRow + deltaRow;
+
+					bool ownPiece = false;
+					bool isCapture = false;
+					while (toColumn >= 0 && toColumn < 8 && toRow >= 0 && toRow < 8 && !ownPiece && !isCapture)
+					{
+						byte to = (byte)(8 * toRow + toColumn);
+						ulong toFilter = (ulong)1 << to;
+
+						if ((ownPiecesAt & toFilter) == 0)
+						{
+							isCapture = (opponentPiecesAt & toFilter) != 0;
+							yield return new Move(from, to, isCapture);
+						}
+						ownPiece = (ownPiecesAt & toFilter) != 0;
+
+						toColumn = toColumn + deltaColumn;
+						toRow = toRow + deltaRow;
+					}
+				}
+
+				bishops &= ~filter;
+				if (bishops == 0)
+				{
+					break;
+				}
+			}
+		}
+	}
+}
