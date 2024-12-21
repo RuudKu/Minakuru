fe5406be7a4e303b8e288478a0eae83bc1f4f7f5
Ruud Kuchler <kuru@pggm.nl>
2024-12-21 14:44:52 +0100

    Knight move generator identifies captures and avoids own pieces

--- DEV/Engine.UnitTests/MoveGenerators/KnightMoveGeneratorTests.cs
+++ DEV/Engine.UnitTests/MoveGenerators/KnightMoveGeneratorTests.cs
@@ -32,5 +32,30 @@ namespace Engine.UnitTests.MoveGenerators
 
 			readableMoves.Should().BeEquivalentTo(expected);
 		}
+
+		[TestMethod]
+		public void WithOtherPiecesTest()
+		{
+			var fen = "7k/8/4r3/8/3N4/5B2/8/K7 w - - 0 1";
+			var board = fen.ToBoard();
+
+			var sut = new KnightMoveGenerator();
+
+			var actual = sut.GenerateMove(board, Color.White).ToArray();
+
+			var readableMoves = actual.ToReadableMoves().ToArray();
+			var expected = new ReadableMove[]
+			{
+				new ("d4", "b3", false, false),
+				new ("d4", "b5", false, false),
+				new ("d4", "c2", false, false),
+				new ("d4", "c6", false, false),
+				new ("d4", "e2", false, false),
+				new ("d4", "e6", true, false),
+				new ("d4", "f5", false, false),
+			};
+
+			readableMoves.Should().BeEquivalentTo(expected);
+		}
 	}
 }
---
+++ DEV/MinakuruEngine/MoveGenerators/KnightMoveGenerator.cs
@@ -0,0 +1,54 @@
+﻿namespace Engine.MoveGenerators
+{
+	public class KnightMoveGenerator : IMoveGenerator
+	{
+		public IEnumerable<Move> GenerateMove(Board board, Color color)
+		{
+			var whitePiecesAt = board.WhiteKing | board.WhiteQueens | board.WhiteRooks | board.WhiteBishops | board.WhiteKnights | board.WhitePawns;
+			var blackPiecesAt = board.BlackKing | board.BlackQueens | board.BlackRooks | board.BlackBishops | board.BlackKnights | board.BlackPawns;
+
+			var knights = color == Color.White ? board.WhiteKnights : board.BlackKnights;
+			var ownPiecesAt = color == Color.White ? whitePiecesAt : blackPiecesAt;
+			var opponentPiecesAt = color == Color.White ? blackPiecesAt : whitePiecesAt;
+
+			for (byte from = 0; from < 64; from++)
+			{
+				var filter = (ulong)1 << from;
+				if ((knights & filter) != 0)
+				{
+					int fromColumn = from % 8;
+					int fromRow = from / 8;
+
+					int toColumn;
+					int toRow;
+
+					for (int option = 0; option < 8; option++)
+					{
+						int deltaColumn = new int[] { -2, -2, -1, -1, +1, +1, +2, +2 }[option];
+						int deltaRow = new int[] { -1, +1, -2, +2, -2, 2, -1, 1 }[option];
+						toColumn = fromColumn + deltaColumn;
+						toRow = fromRow + deltaRow;
+
+						if (toColumn >= 0 && toColumn < 8 && toRow >= 0 && toRow < 8)
+						{
+							byte to = (byte)(8 * toRow + toColumn);
+							ulong toFilter = (ulong)1 << to;
+
+							if ((ownPiecesAt & toFilter) == 0)
+							{
+								bool isCapture = (opponentPiecesAt & toFilter) != 0;
+								yield return new Move(from, to, isCapture);
+							}
+						}
+					}
+
+					knights &= ~filter;
+					if (knights == 0)
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+}
