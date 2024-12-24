df3b7cdcc45d27817ce81233888783d3a934a176
Ruud Kuchler <github@ruudkuchler.nl>
2024-12-24 13:13:13 +0100

    Add Knight threat checker

---
+++ DEV/Engine.UnitTests/ThreatCheckers/KnightThreatCheckerTests.cs
@@ -0,0 +1,34 @@
+﻿using Engine.ThreatCheckers;
+using FluentAssertions;
+
+namespace Engine.UnitTests.ThreatCheckers;
+
+[TestClass]
+public class KnightThreatCheckerTests
+{
+	[DataRow(ColoredPiece.BlackKnight, "b3", true, DisplayName = "Nb3")]
+	[DataRow(ColoredPiece.BlackKnight, "b5", true, DisplayName = "Nb5")]
+	[DataRow(ColoredPiece.BlackKnight, "c2", true, DisplayName = "Nc2")]
+	[DataRow(ColoredPiece.BlackKnight, "c6", true, DisplayName = "Nc6")]
+	[DataRow(ColoredPiece.BlackKnight, "e2", true, DisplayName = "Ne2")]
+	[DataRow(ColoredPiece.BlackKnight, "f3", true, DisplayName = "Nf3")]
+	[DataRow(ColoredPiece.BlackKnight, "f5", true, DisplayName = "Nf5")]
+	[DataRow(ColoredPiece.BlackKnight, "e6", true, DisplayName = "Ne6")]
+	[DataRow(ColoredPiece.BlackKnight, "b2", false, DisplayName = "Nb2")]
+	[DataRow(ColoredPiece.BlackKnight, "g7", false, DisplayName = "Ng7")]
+	[DataTestMethod]
+	public void KingOnD4Tests(ColoredPiece coloredPiece, string fieldName, bool expected)
+	{
+		var fen = "6k1/8/5r2/8/3K4/8/1B6/8 w - - 0 1";
+		var board = fen.ToBoard();
+		var sut = new KnightThreatChecker();
+		byte kingFieldNo = "d4".ToFieldNo();
+		byte attackerFieldNo = fieldName.ToFieldNo();
+		board.SetColoredPieceAt(attackerFieldNo, coloredPiece);
+
+		var actual = sut.IsUnderAttack(board, kingFieldNo, Color.Black);
+
+		actual.Should().Be(expected);
+	}
+}
+
--- DEV/MinakuruEngine/ThreatCheckers/KnightThreatChecker.cs
+++ DEV/MinakuruEngine/ThreatCheckers/KnightThreatChecker.cs
@@ -1,11 +1,32 @@
-﻿
-namespace Engine.ThreatCheckers
+﻿namespace Engine.ThreatCheckers
 {
 	public class KnightThreatChecker : IThreatChecker
-	{
-		public bool IsUnderAttack(Board board, byte targetFieldNo, Color color)
-		{
-			return false;
-		}
-	}
+    {
+        public bool IsUnderAttack(Board board, byte targetFieldNo, Color color)
+        {
+            var knights = color == Color.White ? board.WhiteKnights : board.BlackKnights;
+
+            var toColumnNo = targetFieldNo % 8;
+            var toRowNo = targetFieldNo / 8;
+
+            int fromColumn = 0;
+            int fromRow = 0;
+            ulong knightsFilter = 0L;
+            for (int option = 0; option < 8; option++)
+            {
+                int deltaColumn = new int[] { -2, -2, -1, -1, +1, +1, +2, +2 }[option];
+                int deltaRow = new int[] { -1, +1, -2, +2, -2, 2, -1, 1 }[option];
+                fromColumn = toColumnNo + deltaColumn;
+                fromRow = toRowNo + deltaRow;
+
+                if (fromColumn >= 0 && fromColumn < 8 && fromRow >= 0 && fromRow < 8)
+                {
+                    byte fromFieldNo = (byte)(8 * fromRow + fromColumn);
+                    ulong fromFilter = (ulong)1 << fromFieldNo;
+                    knightsFilter |= fromFilter;
+                }
+            }
+            return (knights & knightsFilter) != 0;
+        }
+    }
 }
