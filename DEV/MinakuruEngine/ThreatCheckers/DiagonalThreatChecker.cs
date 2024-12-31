33c604aa03f65fdb37aa7e172d6b4083895ed046
Ruud Kuchler <github@ruudkuchler.nl>
2024-12-31 20:55:03 +0100

    Diagonal threat checker partially via bitmasks

--- DEV/MinakuruEngine/ThreatCheckers/DiagonalThreatChecker.cs
+++ DEV/MinakuruEngine/ThreatCheckers/DiagonalThreatChecker.cs
@@ -1,44 +1,34 @@
-﻿namespace Minakuru.Engine.ThreatCheckers;
+﻿using Minakuru.Engine.Fields;
+
+namespace Minakuru.Engine.ThreatCheckers;
 
 public class DiagonalThreatChecker : IThreatChecker
 {
+	private static readonly ulong[,][] _bitmasks = DiagonalBitmasks.DiagonalFieldBitmasks;
+
 	public bool IsUnderAttack(Board board, byte targetFieldNo, Color attackedByColor)
 	{
 		var piecesAt = board.Pieces;
 
 		var opponentBishopsQueens = attackedByColor == Color.White ? board.WhiteQueens | board.WhiteBishops : board.BlackQueens | board.BlackBishops;
 
-		var toColumnNo = targetFieldNo % 8;
-		var toRowNo = targetFieldNo / 8;
-
-		int currentColumn;
-		int currentRow;
-
 		for (int direction = 0; direction < 4; direction++)
 		{
-			int deltaColumn = new int[] { -1, -1, +1, +1 }[direction];
-			int deltaRow = new int[] { -1, +1, -1, +1 }[direction];
-			currentColumn = toColumnNo + deltaColumn;
-			currentRow = toRowNo + deltaRow;
-
-			bool ownPiece = false;
 			bool occupied = false;
-			while (currentColumn >= 0 && currentColumn < 8 && currentRow >= 0 && currentRow < 8 && !ownPiece && !occupied)
+			ulong[] fieldIds = _bitmasks[targetFieldNo, direction];
+			byte i = 0;
+			while (!occupied && i < fieldIds.Length)
 			{
-				byte to = (byte)(8 * currentRow + currentColumn);
-				ulong toFilter = (ulong)1 << to;
-
-				if ((piecesAt & toFilter) != 0)
+				var fieldId = fieldIds[i];
+				if ((piecesAt & fieldId) != 0)
 				{
-					if ((opponentBishopsQueens & toFilter) != 0)
+					if ((opponentBishopsQueens & fieldId) != 0)
 					{
 						return true;
 					}
 					occupied = true;
 				}
-
-				currentColumn += deltaColumn;
-				currentRow += deltaRow;
+				i++;
 			}
 		}
 
