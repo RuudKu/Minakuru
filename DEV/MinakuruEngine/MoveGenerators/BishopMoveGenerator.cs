cf17a28ea722245309ac4d6b4bd09aaa65fad48c
Ruud Kuchler <github@ruudkuchler.nl>
2025-01-01 21:09:55 +0100

    Bishop move generator via pre-calculated fields

--- DEV/MinakuruEngine/MoveGenerators/BishopMoveGenerator.cs
+++ DEV/MinakuruEngine/MoveGenerators/BishopMoveGenerator.cs
@@ -1,59 +1,52 @@
 ﻿using System.Numerics;
+using Minakuru.Engine.Fields;
 
 namespace Minakuru.Engine.MoveGenerators;
 
 public class BishopMoveGenerator : IMoveGenerator
 {
+	private static readonly byte[,][] _diagonalPerDirectionFieldNos = DiagonalFieldNo.DiagonalPerDirectionFieldNos;
+
 	public MoveList GenerateMove(Board board)
 	{
 		MoveList moveList = [];
 		var color = board.ColorToMove;
-		var whitePiecesAt = board.WhitePieces;
-		var blackPiecesAt = board.BlackPieces;
 
 		var bishops = color == Color.White ? board.WhiteBishops : board.BlackBishops;
 		var workingCopy = bishops;
-		var ownPiecesAt = color == Color.White ? whitePiecesAt : blackPiecesAt;
-		var opponentPiecesAt = color == Color.White ? blackPiecesAt : whitePiecesAt;
+
+		(ulong ownPiecesAt, ulong opponentPiecesAt) = color == Color.White ? (board.WhitePieces, board.BlackPieces) : (board.BlackPieces, board.WhitePieces);
 
 		byte from = 0;
 		while (workingCopy != 0UL)
 		{
-			byte trailingZeroes = (byte) BitOperations.TrailingZeroCount(workingCopy);
+			byte trailingZeroes = (byte)BitOperations.TrailingZeroCount(workingCopy);
+
 			from += trailingZeroes;
 			workingCopy >>= trailingZeroes;
 			var filter = (ulong)1 << from;
 			if ((bishops & filter) != 0)
 			{
-				int fromColumn = from % 8;
-				int fromRow = from / 8;
-
-				int toColumn;
-				int toRow;
-
 				for (int direction = 0; direction < 4; direction++)
 				{
-					int deltaColumn = new int[] { -1, -1, +1, +1 }[direction];
-					int deltaRow = new int[] { -1, +1, -1, +1 }[direction];
-					toColumn = fromColumn + deltaColumn;
-					toRow = fromRow + deltaRow;
+					byte[] fieldNos = _diagonalPerDirectionFieldNos[from, direction];
 
 					bool ownPiece = false;
 					bool isCapture = false;
-					while (toColumn >= 0 && toColumn < 8 && toRow >= 0 && toRow < 8 && !ownPiece && !isCapture)
+					byte fieldIndex = 0;
+					while (fieldIndex < fieldNos.Length && !ownPiece && !isCapture)
 					{
-						byte to = (byte)(8 * toRow + toColumn);
-						ulong toFilter = (ulong)1 << to;
+						var fieldNo = fieldNos[fieldIndex];
+						ulong toFilter = (ulong)1 << fieldNo;
 
 						if ((ownPiecesAt & toFilter) == 0)
 						{
 							isCapture = (opponentPiecesAt & toFilter) != 0;
-							moveList.Add(new Move(from, to, isCapture));
+							moveList.Add(new Move(from, fieldNo, isCapture));
 						}
 						ownPiece = (ownPiecesAt & toFilter) != 0;
 
-						toColumn += deltaColumn;
-						toRow += deltaRow;
+						fieldIndex++;
 					}
 				}
 
